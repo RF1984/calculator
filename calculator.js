810626911bb72d2baa519086c4d5fb60655e0b38
RF1984 <rafaeldcfernandes@gmail.com>
2019-11-17 23:21:38 +0100

    add documentation

--- calculator.js
+++ calculator.js
@@ -1,6 +1,9 @@
 /*
-  In this file you can write your javascript code. Make sure the DOM is loaded before executing your custom code by
-  attaching an appropriate event listener to the document.
+ * Web calculator
+ * variable number will store the numbers that the user pressed
+ * variable result will store results of math operations
+ * variable element is an array to store the numbers
+ * variable operator will store the operators (+, -, * or /) pressed by the user
  */
 
 let number = "";
@@ -8,23 +11,36 @@ let result = 0;
 let element = [];
 let operator = "";
 
+/*
+* Function that gets the element from the HTML where we want to display numbers and results
+* transform the content to display into a number and displays it on the calculator
+*/
 function displayF (item)
 	{
 		let display = document.querySelector("span");
 		display.textContent = Number(item);
 	}
 
+/*Function called when a number is pressed. 
+*Assigns that number to a variable
+*/
 function addNumber (inputNum)
 {
    number += inputNum;
    displayF(number);
 }
+
+/*Function called when backspace button is pressed
+*erases the last number on the display
+*/
 function backSpace()
 {
 	 number = number.slice(0,-1);
 	 displayF(number);
 }
 
+
+//function to add a dot (".") to the number.
 function addDot()
 {
 	for (var i = 0; i < number.length; i++) {
@@ -35,6 +51,11 @@ function addDot()
 	displayF(number);
 }
 
+/*
+* Function that is called when an operator button is pressed (+, -, * or /)
+* checks if it's the first operator used and stores it into a variable 
+* if it's not the first operate used it will call equal() function to display a result
+*/
 function operatorMethod (inputOperator)
 {
 	if (operator) {
@@ -44,6 +65,10 @@ function operatorMethod (inputOperator)
 	number = "";
 	operator = inputOperator;
 }
+
+/*
+* This function adds a number to the array and then do de mathd according to the values in the array and the operator used.
+*/
 function equal ()
 {
 	if (operator) {
